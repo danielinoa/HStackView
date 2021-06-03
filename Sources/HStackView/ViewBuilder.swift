b6144357a9e58af941e1e201ac94690c7ab590f6
Daniel Inoa <danielinoa@gmail.com>
2021-06-03 00:00:26 -0700

    Make build blocks public

--- Sources/HStackView/ViewBuilder.swift
+++ Sources/HStackView/ViewBuilder.swift
@@ -6,10 +6,10 @@ import UIKit
 
 @resultBuilder
 public enum ViewBuilder {
-    static func buildBlock(_ components: Content...) -> Content { components.flatMap(\.content) }
-    static func buildOptional(_ component: Content?) -> Content { component.map { $0.content } ?? [] }
-    static func buildEither(first component: Content) -> Content { component }
-    static func buildEither(second component: Content) -> Content { component }
+    public static func buildBlock(_ components: Content...) -> Content { components.flatMap(\.content) }
+    public static func buildOptional(_ component: Content?) -> Content { component.map { $0.content } ?? [] }
+    public static func buildEither(first component: Content) -> Content { component }
+    public static func buildEither(second component: Content) -> Content { component }
 }
 
 public protocol Content {
