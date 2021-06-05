dcad3947a46b5688c5cd94642f4b39a7ed2cca8a
Daniel Inoa <danielinoa@gmail.com>
2021-06-04 22:26:21 -0700

    Add documentation to NSLayoutConstraint extension

--- Sources/HStackView/Extensions/NSLayoutConstraint.swift
+++ Sources/HStackView/Extensions/NSLayoutConstraint.swift
@@ -6,6 +6,7 @@ import UIKit
 
 extension NSLayoutConstraint {
 
+    /// Sets the constraint priority to the specified value.
     func setPriority(_ priority: UILayoutPriority) -> Self {
         self.priority = priority
         return self
